aae66eb594064f348c1b35578498b88fd727e6b8
Krvz20 <kevinrodriguez000@hotmail.com>
2025-04-10 17:40:19 -0500

    PROYECTO DJANGO

--- tareas/todo/urls.py
+++ tareas/todo/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"), 
-    path("agregar/", views.agregar, name="agregar"),  
+    path("agregar/", views.agregar, name="agregar"),
+     
 ]
